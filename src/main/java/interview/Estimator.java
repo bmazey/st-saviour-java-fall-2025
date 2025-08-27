fbe8a92f6452dd432f1b2568f3a96dc7b4ab5022
brandon mazey <brandon@Mac.localdomain>
2025-08-27 11:51:35 -0400

    adding test cases

---
+++ src/main/java/interview/Estimator.java
@@ -0,0 +1,16 @@
+package interview;
+
+public class Estimator {
+    
+    /*
+     * The round() method accepts a double d and returns an int.
+     * The resulting integer should be rounded up when the decimal is >= .5
+     * Negative numbers should also be rounded up, but the result should remain negative.
+     *  - ex:  1.2 -> 1
+     *  - ex: -3.6 -> -4
+     */
+    public static int round(double d) {
+        // TODO
+        return 0;
+    }
+}
--- src/test/java/interview/OfficeSupplierTest.java
+++ src/test/java/interview/OfficeSupplierTest.java
@@ -1,5 +1,24 @@
 package interview;
 
+import org.junit.jupiter.api.Test;
+
 public class OfficeSupplierTest {
     
+    @Test
+    public static void testOfficeShredder() {
+        assert OfficeSupplier.shredFirstCharacter("Michael").equals("ichael");
+        assert OfficeSupplier.shredFirstCharacter("Pam").equals("am");
+
+        assert OfficeSupplier.shredLastCharacter("Dwight").equals("Dwigh");
+        assert OfficeSupplier.shredLastCharacter("Jim").equals("Ji");
+    }
+
+    @Test
+    public static void testOfficeStapler() {
+        assert OfficeSupplier.stapleToBeginning("ngela", 'A').equals("Angela");
+        assert OfficeSupplier.stapleToBeginning("tanley", 'S').equals("Stanley");
+
+        assert OfficeSupplier.stapleToEnd("Kell", 'y').equals("Kelly");
+        assert OfficeSupplier.stapleToEnd("Meredit", 'h').equals("Meredith");
+    }
 }
