8b9b5e31784086b014fd05fa9ad5eb95487ba0f5
brandon mazey <brandon@Mac.localdomain>
2025-08-27 11:28:24 -0400

    ace the interview

---
+++ src/main/java/interview/SevenEleven.java
@@ -0,0 +1,16 @@
+package interview;
+
+public class SevenEleven {
+    
+    /*
+     * The shop() method accepts an int 'price' and returns a String.
+     *   - If the price is a multiple of 7 the result String should be "seven".
+     *   - If the price is a multiple of 11 the result String should be "eleven".
+     *   - If the price is a multiple of 7 AND 11 the result String should be "seveneleven"
+     *   - If the price is NOT a multiple of either 7 OR 11 the result String should be "".
+     */
+    public static String shop(int price) {
+        // TODO
+        return "";
+    }
+}
---
+++ src/test/java/interview/PasswordTest.java
@@ -0,0 +1,38 @@
+package interview;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * TODO - BONUS +5 points!
+ * 
+ * Your test should cover the following cases:
+ *   - Create a new password and ensure that the first 5 characters are letters.
+ *   - Using the existing password, ensure that the next 4 characters are digits.
+ *   - Using the existing password, ensure that the final character is a symbol.
+ *   - Using the existing password, ensure that the length is 10.
+ *   - Create two new passwords, and ensure that they are not equal.
+ */
+public class PasswordTest {
+
+    @Test
+    public void testPasswordComplexity() {
+        // TODO Check password characters, digits, & symbols at specified positions.
+        // HINT Java's Character class has rich support for classifying unicode symbols.
+        // https://docs.oracle.com/javase/8/docs/api/java/lang/Character.html#isLetter-char-
+        // https://docs.oracle.com/javase/8/docs/api/java/lang/Character.html#isDigit-char-
+        assert true;
+    }
+
+    @Test
+    public void testPasswordLength() {
+        // TODO Ensure that generated password is of length 10.
+        assert true;
+    }
+
+    @Test
+    public void testPasswordUnique() {
+        // TODO Create two passwords and ensure that they do not equal each other.
+        // NOTE We overlook collisions for the purpose of this exercise.
+        assert true;
+    }
+}
