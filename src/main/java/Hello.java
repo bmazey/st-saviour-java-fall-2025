baff1d13fcd2362508e24bfcdc22a3b770fc881e
brandon mazey <brandon@Mac.localdomain>
2025-10-08 12:26:41 -0400

    examples

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,23 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("new dawn, new day!");
+
+        String s = "October";
+
+        // int i = 0;
+        // while(i < s.length()) {
+        //     System.out.println("The character we're on is: " + s.charAt(i));
+        //     i += 1;
+        // }
+
+        // for(int i = 0; i < 100; i+=2) {
+        //     System.out.println("I will not cheat on my CS exam ... " + i);
+        // }
+
+        int i = 0;
+        do {
+            System.out.println("I will not cheat on my CS exam ... " + i);
+            i += 1;
+        } while (i < 100);
     }
 }
