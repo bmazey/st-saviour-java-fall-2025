e08e02e11b48627e4d7bc1466a1fbb38e8242749
bmc2425 <recheriyan@stsaviour.org>
2025-10-24 19:38:57 -0400

    lab 2 donegit add .

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,23 @@ class Hello {
     public static void main(String[] args) {
         // hehehe
         System.out.println("new dawn, new day!");
+
+        int[] numbers = {1, 2, 3, 7, 7, 3, 1, 7};
+
+        int count = 0;
+        for(int i = 0; i < numbers.length; i++)
+            if(numbers[i] == 3 || numbers[i] == 7); {
+                count++;
+            }
     }
 }
+//     int[] result = new int[count];
+//     int position = 0;
+// for(int i = 0; i< numbers.length; i++)} //shorer array than the first one, not the same positions
+//     if(numbers[i] == 3 || numbers[i] == 7); {
+//         result[position] = numbers[i];
+//         position++;
+//     }
+//     for(int i = 0; i < result.length; i++) {
+//         System.out.println("result: " + result[i]);
+//     }
